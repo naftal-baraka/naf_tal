1908f3eb9820a378182b1e8f69fe2781113631cb
naftal-baraka <135145729+naftal-baraka@users.noreply.github.com>
2023-05-31 18:24:39 +0300

    Add files via upload

---
+++ lesson1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>	</title>
+</head>
+<body>
+<?php
+  #this is for variable declaration
+     $num1=10;
+     $num2=15;
+     //arithmetic computations
+     
+     $sum=$num1+$num2;
+     $dif=$num1-$num2;
+     $prod=$num1*$num2;
+     $div=$num1/$num2;
+     $modu=$num1%$num2;
+     
+     #outputs
+     echo "the sum of $num1 and $num2 is:$sum <br>"; 
+     echo "the difference of $num1 and $num2 is:$dif <br>";
+     echo "the product of $num1 and $num2 is:$prod <br>"; 
+     echo "the division of $num1 and $num2 is:$div<br>"; 
+     echo "the modulus of $num1 and $num2 is:$modu <br>"; 
+
+
+?>
+</body>
+</html>
